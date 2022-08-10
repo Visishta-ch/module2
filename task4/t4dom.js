d081bb7f98715c66617297350896d593d872db27
Visishta-ch <Visishtachatla@gmail.com>
2022-08-11 01:01:57 +0530

    task4 commit

---
+++ task4/t4dom.js
@@ -0,0 +1,21 @@
+//getElementByClassName
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'add item 2';
+items[1].style.fontWeight='bold';
+//items[1].style.backgroundColor = 'yellow';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor = 'pink'
+}
+
+for(var i=0;i<items.length-1;i++){
+    items[i].style.fontWeight = 'bold';
+}
+
+items[3].style.backgroundColor = 'green';
+
+items[3].style.color='white';
+items[3].style.fontWeight = 'bold';
