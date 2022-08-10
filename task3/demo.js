8a6093bf2b7cadafb34e0f0aa5052ecc0ef8c66c
Visishta-ch <Visishtachatla@gmail.com>
2022-08-11 00:18:21 +0530

    first commit

---
+++ task3/demo.js
@@ -0,0 +1,26 @@
+//console.log(document);
+
+console.log(document.domain);
+console.log(document.head);
+console.log(document.title);
+console.log(document.body);
+
+console.log(document.links);
+document.title = "docs";
+//chaning title of page
+var h2 = document.getElementById('title');
+h2.style.color = 'green';
+
+document.getElementById('header-title').textContent = 'Hello';
+//document.all is not supported
+
+
+var header = document.getElementById('main-header');
+var htitle = document.getElementById('header-title');
+console.log('htitle');
+htitle.innerText ='Hi Everyone';
+
+htitle.innerHTML = '<h2>Hello</h2>';
+
+header.style.borderBottom = 'solid 3px #000';
+
